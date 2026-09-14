c04418fc19366d6b11f9c21847d786724b3a0287
jrd20 <dahli157@d.umn.edu>
2026-09-14 16:06:06 -0500

    lab 1

--- src/main/java/edu/umn/d/cs1632/Main.java
+++ src/main/java/edu/umn/d/cs1632/Main.java
@@ -8,10 +8,7 @@ public class Main  {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        /**
-        System.out.print("Fil of data to read: ");
-        String fileName = scanner.nextLine();
-        */
+
 
  
 
